ea3804ce6e440dcb3145f6b00ba1a109246f61af
TA, Liu <43956723+105304039@users.noreply.github.com>
2023-04-12 12:13:30 +0800

    Update admin.py

--- django_admin/trips/admin.py
+++ django_admin/trips/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
-
-#我們需要在讓 Django 知道，有哪些 Model 需要管理後台。修改 trips app 裡的 admin.py，並註冊 Post 這個 Model：
-
 
 from .models import Post, CAL1
 
